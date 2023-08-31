4f90859a54dc14b82b5f00bfddbc7e75fd3c9062
Hellraiser997 <felipesg03@gmail.com>
2023-08-31 16:05:11 -0300

    Feat: finalizado script; criado prompt para entrada, função com loop, coleta e organização por ordem alfabética dos dados/propriedades.

--- ordenador.js
+++ ordenador.js
@@ -6,4 +6,40 @@ const rl = readline.createInterface({
 });
 
 
+function entradaUsuario(prompt) {
+    return new Promise(resolve => {
+        rl.question(prompt, resolve)
+    })
+}
 
+async function coletaPropriedades() {
+    const propriedades = [];
+    while (true) {
+        const propriedade = await entradaUsuario(
+            'Digite aqui as propriedades desejadas:'
+        )
+        if (propriedade.toLocaleUpperCase() === 'SAIR') {
+            return propriedades;
+        } else if (isNaN(propriedade)) {
+            propriedades.push(propriedade);
+        }
+    }
+}
+
+function mostrarPropriedadesOrdenadas(propriedades) {
+    const propriedadesOrdenadas = propriedades.slice().sort();
+    console.log("Propriedades CSS ordenadas A-Z:");
+    propriedadesOrdenadas.forEach(prop => console.log(prop))
+}
+
+async function main() {
+    console.log('Ordenador de propriedades CSS');
+    const propriedades = await coletaPropriedades();
+    mostrarPropriedadesOrdenadas(propriedades);
+    rl.close();
+}
+
+main().catch(error => {
+    console.log("Ocorreu um erro:", error.message);
+    rl.close
+})
